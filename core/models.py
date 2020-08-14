d59d39fb1ebc9bc982cc4b08b5e880151f9973e0
scarter13 <scarter13@gmail.com>
2020-08-14 15:56:59 -0400

    first steps on models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Contact(models.Model):
+    BOOSTER = 'BOOSTER'
+    OBLIGATE = 'OBLIGATE'
+    CURMUDGEON = 'CURMUDGEON'
+    NOT_RATED = 'NR'
+    STATUS_CHOICES =[
+        (BOOSTER, 'Booster'),
+        (OBLIGATE, 'Obligate'),
+        (CURMUDGEON, 'Curmudgeon'),
+        (NOT_RATED, 'Not Yet Rated'),
+    ]
+    ALUMNI = 'ALUMNI'
+    ADVOCATE = 'ADVOCATE'
+    UNKNOWN = 'UNKNOWN'
+    RELATIONSHIP_CHOICES =[
+        (ALUMNI, 'Alumni'),
+        (ADVOCATE, 'Advocate'),
+        (UNKNOWN, 'Not Known'),
+    ]
+    name = models.CharField(max_length=255)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.Charfield(max_length=14, blank=True, null=True)
+    linkedin = models.CharField(max_length=255, blank=True, null=True)
+    twitter = models.CharField(max_length=255, blank=True, null=True)
+    status = models.CharField(max_length=13, choices=STATUS_CHOICES, default=NOT_RATED)
+    relationship = models.CharField(max_length=8, choices=RELATIONSHIP_CHOICES, default=UNKNOWN)
+    first_contact = models.DateField
+
+
+
+
